7ec509979a2b755840ebdf3c71fe693fab600941
xnxaa <vivimusfiroh83@gmail.com>
2023-06-16 07:34:42 +0700

    first

---
+++ coba/src/components/PostForm.jsx
@@ -0,0 +1,28 @@
+import { Button, Card, CardActions, CardContent, CircularProgress, TextField, Typography } from '@mui/material'
+import { useState } from 'react'
+import usePostService from '../services/rest-client/index'
+
+export const PostForm = () => {
+    const [post, setPost] = useState()
+    const { createPosts, loading } = usePostService()
+
+        const handleCreatePost = async () => {
+            await createPosts(post)
+            setPost('')
+        }
+    
+    return <Card>
+        <CardContent>
+        <Typography variant='h6'>
+            Post new Feed
+        </Typography>
+            <TextField multiline placeholder='post' value={post} onChange={({ target: { value } }) => setPost(value)} />
+        </CardContent>
+        <CardActions>
+            <Button variant='contained' onClick={handleCreatePost}>
+                { loading && <CircularProgress/> }
+                Post
+                </Button>
+        </CardActions>
+    </Card>
+}
---
+++ coba/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {GoogleOAuthProvider} from "@react-oauth/google"
+import config from "../src/config/index"
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <GoogleOAuthProvider clientId={config.google.clientId}>
+    <App />
+    </GoogleOAuthProvider>
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
